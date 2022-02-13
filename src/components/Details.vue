d653895d3e3218c6ac41f9716c12be599508e19a
throuz <64568129+throuz@users.noreply.github.com>
2022-02-14 02:57:25 +0800

    Optimize Details

--- src/components/Details.vue
+++ src/components/Details.vue
@@ -12,76 +12,7 @@
           Country Details
         </v-card-title>
 
-        <v-simple-table>
-          <tbody>
-            <tr v-for="(value, name, i) in countryData" :key="i">
-              <template
-                v-if="typeof value === 'object' && !Array.isArray(value)"
-              >
-                <td>{{ name }}</td>
-                <td class="pr-0 pl-0">
-                  <v-simple-table>
-                    <tbody>
-                      <tr v-for="(value2, name2, j) in value" :key="j">
-                        <template
-                          v-if="
-                            typeof value2 === 'object' && !Array.isArray(value2)
-                          "
-                        >
-                          <td>{{ name2 }}</td>
-                          <td class="pr-0 pl-0">
-                            <v-simple-table>
-                              <tbody>
-                                <tr
-                                  v-for="(value3, name3, k) in value2"
-                                  :key="k"
-                                >
-                                  <template
-                                    v-if="
-                                      typeof value3 === 'object' &&
-                                      !Array.isArray(value3)
-                                    "
-                                  >
-                                    <td>{{ name3 }}</td>
-                                    <td class="pr-0 pl-0">
-                                      <v-simple-table>
-                                        <tbody>
-                                          <tr
-                                            v-for="(value4, name4, l) in value3"
-                                            :key="l"
-                                          >
-                                            <td>{{ name4 }}</td>
-                                            <td>{{ value4 }}</td>
-                                          </tr>
-                                        </tbody>
-                                      </v-simple-table>
-                                    </td>
-                                  </template>
-                                  <template v-else>
-                                    <td>{{ name3 }}</td>
-                                    <td>{{ value3 }}</td>
-                                  </template>
-                                </tr>
-                              </tbody>
-                            </v-simple-table>
-                          </td>
-                        </template>
-                        <template v-else>
-                          <td>{{ name2 }}</td>
-                          <td>{{ value2 }}</td>
-                        </template>
-                      </tr>
-                    </tbody>
-                  </v-simple-table>
-                </td>
-              </template>
-              <template v-else>
-                <td>{{ name }}</td>
-                <td>{{ value }}</td>
-              </template>
-            </tr>
-          </tbody>
-        </v-simple-table>
+        <DetailsTable :tableData="countryData" />
 
         <v-divider></v-divider>
 
@@ -95,7 +26,15 @@
 </template>
 
 <script>
+import DetailsTable from "./DetailsTable";
+
 export default {
+  name: "Details",
+
+  components: {
+    DetailsTable,
+  },
+
   props: {
     countryName: {
       type: String,
@@ -114,9 +53,3 @@ export default {
   },
 };
 </script>
-
-<style scoped>
-td {
-  border-left: thin solid rgba(0, 0, 0, 0.12);
-}
-</style>
---
+++ src/components/DetailsTable.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-simple-table>
+    <tbody>
+      <tr v-for="(value, name, i) in tableData" :key="i">
+        <template v-if="typeof value === 'object' && !Array.isArray(value)">
+          <td>{{ name }}</td>
+          <td class="pr-0 pl-0 no-border-left">
+            <DetailsTable :tableData="value" />
+          </td>
+        </template>
+        <template v-else>
+          <td>{{ name }}</td>
+          <td>{{ Array.isArray(value) ? value.join(" 、") : value }}</td>
+        </template>
+      </tr>
+    </tbody>
+  </v-simple-table>
+</template>
+
+<script>
+export default {
+  name: "DetailsTable",
+
+  props: {
+    tableData: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+};
+</script>
+
+<style scoped>
+td {
+  border-left: thin solid rgba(0, 0, 0, 0.12);
+}
+.no-border-left {
+  border-left: none;
+}
+</style>
